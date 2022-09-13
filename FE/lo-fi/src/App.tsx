a74e28434d410fb0b993cfb927bc903ea00ddc63
Sohee Cheon <csh5156@gmail.com>
2022-09-13 17:43:03 +0900

    [S07P22B102-79] FEAT: In Main Page, Kakao Map insert and Search bar create

--- FE/lo-fi/src/App.tsx
+++ FE/lo-fi/src/App.tsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import FooterBar from './components/FooterBar';
 import MainPage from './pages/MainPage';
+import SearchPage from './pages/SearchPage';
 
 function App() {
   return (
     <div className="App">
-      <MainPage />
-
-      <FooterBar/>
+      <Router>
+        <Routes>
+          <Route path='/' element={<MainPage />}/>
+          <Route path='/search' element={<SearchPage />}/>
+        </Routes>
+        <FooterBar/>
+      </Router>
     </div>
   );
 }
