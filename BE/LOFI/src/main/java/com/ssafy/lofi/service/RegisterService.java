434a22e2ca4967b2393f2499855d50880df0fe79
허유민 <fodleldjs@naver.com>
2022-09-16 00:35:00 +0900

    [S07P22B102-110] FEAT: 마이페이지기능추가

--- BE/LOFI/src/main/java/com/ssafy/lofi/service/RegisterService.java
+++ BE/LOFI/src/main/java/com/ssafy/lofi/service/RegisterService.java
@@ -4,6 +4,7 @@ import com.ssafy.lofi.db.entity.User;
 import com.ssafy.lofi.db.repository.UserRepository;
 import com.ssafy.lofi.dto.UserRole;
 import com.ssafy.lofi.dto.request.SignUpDto;
+import com.ssafy.lofi.dto.response.UserDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,5 +23,14 @@ public class RegisterService {
                 .provider(signUpDto.getProvider())
                 .build();
         return userRepository.save(user);
+    }
+
+    public UserDto mypage(String email) {
+        User user = userRepository.findByEmail(email).orElseThrow(() -> new IllegalArgumentException("해당 이메일을 가진 유저가 없습니다. email=" + email));;
+        UserDto userDto = new UserDto();
+        userDto.setId(user.getId());
+        userDto.setEmail(user.getEmail());
+        userDto.setPoint(user.getPoint());
+        return userDto;
     }
 }
