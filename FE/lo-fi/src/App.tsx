060a8fcd7cf16e65cdd9c2512de7a7c0d5f5119c
정승우 <tmddn1008@naver.com>
2022-09-26 11:15:49 +0900

    [S07P22B102-208] feat:login

--- FE/lo-fi/src/App.tsx
+++ FE/lo-fi/src/App.tsx
@@ -7,9 +7,9 @@ import SearchPage from './pages/SearchPage';
 import DetailPage from './pages/DetailPage';
 import AddPage from './pages/AddPage';
 import AddDetailPage from './pages/AddDetailPage';
-import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
-import KakaoLogin from './pages/KakaoLogin';
+import LoginPage from './pages/LoginPage';
+import GoogleLogin from './pages/GoogleLogin';
 
 
 
@@ -20,12 +20,12 @@ function App() {
         <Routes>
           <Route path='/' element={<MainPage />}/>
           <Route path='/login' element={<LoginPage />}/>
-          <Route path='/kakaoLogin' element={<KakaoLogin />}/>
           <Route path='/register' element={<RegisterPage />}/>
           <Route path='/search' element={<SearchPage />}/>
           <Route path='/search/:id' element={<DetailPage />}/>
           <Route path='/add' element={<AddPage />}/>
           <Route path='/add/:category' element={<AddDetailPage/>}/>
+          <Route path='/oauth2/:token' element={<GoogleLogin/>}/>
         </Routes>
         <FooterBar/>
       </Router>
