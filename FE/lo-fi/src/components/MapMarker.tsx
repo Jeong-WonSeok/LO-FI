58ca085c12fab2f689258ef6677a192d2d9e7c16
Yenno <csh5156@gmail.com>
2022-09-26 11:23:53 +0900

    FEAT: 지도 클릭하면 주소 정보 넘어감

--- FE/lo-fi/src/components/MapMarker.tsx
+++ FE/lo-fi/src/components/MapMarker.tsx
@@ -1,16 +1,22 @@
-import React, {useEffect} from 'react'
+import { info } from 'console';
+import React, {useEffect, useState, useRef} from 'react'
 import './MapMarker.css'
+import {Iprops} from '../pages/AddDetailPage';
 
 const kakao = (window as any).kakao
 
-export default function MapMarker() {
+export default function MapMarker(props: any) {
   let lon = 0;
   let lat = 0;
-  
+  const [address, setAddress] = useState('')
+  const el = useRef<HTMLDivElement | null> (null);
+
   useEffect(() => {
     async function fecthmap() {
+    window.addEventListener('click', handleCloseModal)
     getLocation()
     setTimeout(() => {
+    
     var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
     mapOption = {
         center: new kakao.maps.LatLng(lat, lon), // 지도의 중심좌표
@@ -23,39 +29,42 @@ export default function MapMarker() {
     // 주소-좌표 변환 객체를 생성합니다
     var geocoder = new kakao.maps.services.Geocoder()
   
-    var marker = new kakao.maps.Marker(), // 클릭한 위치를 표시할 마커입니다
-    infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
+    var marker = new kakao.maps.Marker(); // 클릭한 위치를 표시할 마커입니다
+    // infowindow = new kakao.maps.InfoWindow({zindex:1}); // 클릭한 위치에 대한 주소를 표시할 인포윈도우입니다
   
     // 현재 지도 중심좌표로 주소를 검색해서 지도 좌측 상단에 표시합니다
-    searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+    // searchAddrFromCoords(map.getCenter(), displayCenterInfo);
   
     // 지도를 클릭했을 때 클릭 위치 좌표에 대한 주소정보를 표시하도록 이벤트를 등록합니다
     // 여기를 하단에 위치를 바꾸는 걸로 하자!
     kakao.maps.event.addListener(map, 'click', function(mouseEvent: any) {
       searchDetailAddrFromCoords(mouseEvent.latLng, function(result: any, status: any) {
         if (status === kakao.maps.services.Status.OK) {
-          var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
-          detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
+          var infoDiv = document.getElementById('centerAddr') as HTMLDivElement
+          infoDiv.innerText = result[0].address.address_name
+          setAddress(result[0].address.address_name)
+          // var detailAddr = !!result[0].road_address ? '<div>도로명주소 : ' + result[0].road_address.address_name + '</div>' : '';
+          // detailAddr += '<div>지번 주소 : ' + result[0].address.address_name + '</div>';
               
-          var content = '<div class="bAddr">' + 
-            detailAddr + 
-            '</div>';
+          // var content = '<div class="bAddr">' + 
+          //   detailAddr + 
+          //   '</div>';
   
           // 마커를 클릭한 위치에 표시합니다 
           marker.setPosition(mouseEvent.latLng);
           marker.setMap(map);
   
           // 인포윈도우에 클릭한 위치에 대한 법정동 상세 주소정보를 표시합니다
-          infowindow.setContent(content);
-          infowindow.open(map, marker);
+          // infowindow.setContent(content);
+          // infowindow.open(map, marker);
         }   
       });
     });
   
     // 중심 좌표나 확대 수준이 변경됐을 때 지도 중심 좌표에 대한 주소 정보를 표시하도록 이벤트를 등록합니다
-    kakao.maps.event.addListener(map, 'idle', function() {
-        searchAddrFromCoords(map.getCenter(), displayCenterInfo);
-    });
+    // kakao.maps.event.addListener(map, 'idle', function() {
+    //     searchAddrFromCoords(map.getCenter(), displayCenterInfo);
+    // });
   
     function searchAddrFromCoords(coords: any, callback: any) {
         // 좌표로 행정동 주소 정보를 요청합니다
@@ -68,23 +77,26 @@ export default function MapMarker() {
     }
   
     // 지도 좌측상단에 지도 중심좌표에 대한 주소정보를 표출하는 함수입니다
-    function displayCenterInfo(result: any, status: any) {
-      if (status === kakao.maps.services.Status.OK) {
-        var infoDiv = document.getElementById('centerAddr') as HTMLDivElement;
+    // function displayCenterInfo(result: any, status: any) {
+    //   if (status === kakao.maps.services.Status.OK) {
+    //     var infoDiv = document.getElementById('centerAddr') as HTMLDivElement;
   
-        for(var i = 0; i < result.length; i++) {
-          // 행정동의 region_type 값은 'H' 이므로
-          if (result[i].region_type === 'H') {
-              infoDiv.innerHTML = result[i].address_name;
-              break;
-          }
-        }
-      }    
-    }
+    //     for(var i = 0; i < result.length; i++) {
+    //       // 행정동의 region_type 값은 'H' 이므로
+    //       if (result[i].region_type === 'H') {
+    //           infoDiv.innerHTML = result[i].address_name;
+    //           break;
+    //       }
+    //     }
+    //   }    
+    // }
     }, 100); 
     }
-    
     fecthmap()
+
+    return(
+      window.removeEventListener('click', handleCloseModal)
+    )
   }, [lat, lon])
 
   const getLocation = () => {
@@ -110,13 +122,32 @@ export default function MapMarker() {
     }
   }
 
+  
+  const handleCloseModal = (e: any) => {
+    console.log(el.current)
+    console.log(e.target)
+    if (!el.current || !el.current.contains(e.target)) {
+      props.closeModal()
+    }
+  }
+
+  // 부모 컴포넌트로 데이터 보내기
+  const sendData = () => {
+    console.log(address)
+    console.log(props.getAddress)
+    props.getAddress(address)
+  }
+
   return (
-  <div className="map_wrap">
-    <div>
+  <div className="map_wrap" style={{zIndex: "8"}} ref={el}>
+    <div style={{zIndex: "10"}}>
       <div id="map" style={{width:'85vw', height: '85vh', overflow:'hidden'}}></div>
-      <div className="hAddr">
-        <p className="title">지도중심기준 행정동 주소정보</p>
-        <p id="centerAddr"></p>
+      <div className="hAddr" style={{display: "flex", justifyContent: "space-between", flexDirection: "row"}}>
+        <div>
+          <p className="title">주소</p>
+          <p id="centerAddr"></p>
+        </div>
+        <button onClick={sendData} style={{borderRadius: "20px", background: "#B4E0D7", width: "50px", padding: "5px"}}>완료</button>
       </div>
     </div>
   </div>
