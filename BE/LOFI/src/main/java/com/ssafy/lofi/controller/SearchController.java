44c6210d5b8aebe9bce18289adf817e66e793a36
junghun95 <79126498+junghun95@users.noreply.github.com>
2022-09-28 13:58:40 +0900

    FIX: API데이터 업데이트 오류 수정

--- BE/LOFI/src/main/java/com/ssafy/lofi/controller/SearchController.java
+++ BE/LOFI/src/main/java/com/ssafy/lofi/controller/SearchController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.xml.ws.Response;
 
 @RequiredArgsConstructor
 @RestController
