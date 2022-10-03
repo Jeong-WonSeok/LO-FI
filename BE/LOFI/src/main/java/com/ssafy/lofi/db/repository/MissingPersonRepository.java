13abc07f5914d0d3417e8f806ed71f457e0822ff
허유민 <fodleldjs@naver.com>
2022-10-03 20:59:47 +0900

    [S07P22B102-149] FEAT: 현재위치기반 리스트 조회 API

--- BE/LOFI/src/main/java/com/ssafy/lofi/db/repository/MissingPersonRepository.java
+++ BE/LOFI/src/main/java/com/ssafy/lofi/db/repository/MissingPersonRepository.java
@@ -6,9 +6,15 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MissingPersonRepository extends JpaRepository<MissingPerson, Long> {
     @Modifying
     @Query(value = "truncate table missing_person", nativeQuery = true)
     void truncateMissingPerson();
+
+    @Query(nativeQuery = true, value = "select m.* from missing_person as m " +
+            "where (6371*acos(cos(radians(:lat))*cos(radians(m.latitude))*cos(radians(m.longitude)-radians(:lon))+sin(radians(:lat))*sin(radians(m.latitude)))) <= 0.3")
+    List<MissingPerson> findAllByLatLon(double lat, double lon);
 }
