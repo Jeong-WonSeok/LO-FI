97dba083e4161509261a5edbf09c0be013e95d6a
Yenno <csh5156@gmail.com>
2022-10-05 13:20:10 +0900

    FEAT: axios 요청 변경

--- FE/lo-fi/src/api/axios.tsx
+++ FE/lo-fi/src/api/axios.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: "http://j7b102.p.ssafy.io:8085/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "http://j7b102.p.ssafy.iog:8085/",
+  // baseURL: "http://localhost:8080/",
   headers: {
     Authorization: 'Bearer ' + localStorage.getItem('token')!
   },  
