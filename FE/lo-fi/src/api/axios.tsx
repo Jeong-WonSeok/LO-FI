c85bbc22682ed7fd61ecfd24fcbc10f31f716cff
Sohee Cheon <csh5156@gmail.com>
2022-10-06 09:03:01 +0900

    Fix: axios 요청 수정

--- FE/lo-fi/src/api/axios.tsx
+++ FE/lo-fi/src/api/axios.tsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  // baseURL: "https://j7b102.p.ssafy.io/",
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://j7b102.p.ssafy.io/",
+  // baseURL: "http://localhost:8080/",
   headers: {
     Authorization: 'Bearer ' + localStorage.getItem('token')!
   },  
