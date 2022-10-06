d6617f4307136782f5e895759d0b86b46a3d2fc0
Sohee Cheon <csh5156@gmail.com>
2022-10-06 20:04:21 +0900

    FEAT: 검색기능 구현

--- FE/lo-fi/src/components/SearchBar.tsx
+++ FE/lo-fi/src/components/SearchBar.tsx
@@ -8,25 +8,35 @@ import { useAppDispatch, useAppSelector } from '../hooks/reduxHook';
 import './SearchBar.css'
 
 export default function SearchBar(props: any) {
-  const { category } = useAppSelector(state => state.mainData);
+  const { category, search, pending, search_text } = useAppSelector(state => state.mainData);
   const dispatch = useAppDispatch();
 
   const navigate = useNavigate();
-  const [SearchText, setSearchText] = useState("");
+  const [SearchText, setSearchText] = useState(search_text);
 
+  const xButton = document.getElementById('x_button')
   const handleChange = (e: any) => {
     setSearchText(e.target.value)
 
     // 빈값이라면 검색 종료 및 원래 데이터로 돌려놓기
-    if (!SearchText) {
+    if (e.target.value === "") {
+      xButton?.classList.add('not_search')
       dispatch(stopSearch())
       return
     }
+
+    
+    xButton?.classList.remove('not_search')
+  }
+
+  const endSearch = () => {
+    setSearchText('')
+    dispatch(stopSearch())
+    xButton?.classList.add('not_search')
   }
 
   // 엔터를 입력하면 검색이 실시
   const Search = (e: any) => {
-    console.log(SearchText)
     if (e.key === "Enter") {
       dispatch(searchData(category, SearchText))
     }
@@ -44,6 +54,14 @@ export default function SearchBar(props: any) {
     case "map":
       return (
         <div className='search_top_nav'>
+          { search && pending && 
+            <div id="searching">
+              <div className="loading-container">
+              <div className="loading"></div>
+              <div id="loading-text">searching</div>
+              </div>
+            </div>
+            }
           <div className='search_map' onClick={go}>
             <img src={list} alt="" width={35} height={35} />
             <span>목록으로</span>
@@ -51,12 +69,22 @@ export default function SearchBar(props: any) {
           <div className='search_box'>
             <img src={search_icon} alt="" width={20} height={20}/>
             <input className="search_input" placeholder="키워드를 최대 3가지 입력해주세요" type="text" value={SearchText} onChange={handleChange} onKeyDown={Search}/>
+            {!SearchText && <div className='stop_search not_search' id="x_button" onClick={endSearch}>X</div>}
+            {SearchText && <div className='stop_search' id="x_button" onClick={endSearch}>X</div>}
           </div>
         </div>
       );
     case "list":
       return (
         <div className='search_top_nav'>
+          { search && pending && 
+            <div id="searching">
+              <div className="loading-container">
+              <div className="loading"></div>
+              <div id="loading-text">searching</div>
+              </div>
+            </div>
+            }
           <div className='search_map' onClick={go}>
             <img src={map} alt="" width={35} height={35} />
             <span>지도로</span>
@@ -64,6 +92,8 @@ export default function SearchBar(props: any) {
           <div className='search_box'>
             <img src={search_icon} alt="" width={20} height={20}/>
             <input className="search_input" placeholder="키워드를 최대 3가지 입력해주세요" type="text" value={SearchText} onChange={handleChange} onKeyDown={Search}/>
+            {!SearchText && <div className='stop_search not_search' id="x_button" onClick={endSearch}>X</div>}
+            {SearchText && <div className='stop_search' id="x_button" onClick={endSearch}>X</div>}
           </div>
         </div>
       );
